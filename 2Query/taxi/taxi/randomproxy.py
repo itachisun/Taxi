5e98671618f1f4ae727ef1713ae04c0906c95b08
Larry Xiao <xiaodi@sjtu.edu.cn>
2014-09-20 08:00:20 +0800

    experiment with license

--- 2Query/taxi/taxi/randomproxy.py
+++ 2Query/taxi/taxi/randomproxy.py
@@ -18,6 +18,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+# https://github.com/aivarsk/scrapy-proxies
+
 import re
 import random
 import base64
